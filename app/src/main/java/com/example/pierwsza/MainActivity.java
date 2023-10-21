ebada27cec5b1fd1a40328604af0f03b8907ce67
Adam Karczewski <akarczewski22@gmail.com>
2023-10-21 12:01:39 +0200

    Layout

---
+++ app/src/main/java/com/example/pierwsza/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pierwsza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_main);
+        //setContentView(R.layout.vertical);
+        //setContentView(R.layout.verical2);
+       // setContentView(R.layout.constraintlayout);
+        //setContentView(R.layout.constraintlayout2);
+       // setContentView(R.layout.tablelayout);
+        //setContentView(R.layout.gridlayout);
+        setContentView(R.layout.multilayout);
+    }
+}
